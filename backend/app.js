c951a766e64c6611f724ff2377f90f65b939e737
sairam5686 <jrsairam5686@gmail.com>
2025-07-29 13:13:20 +0530

    --oppertunities section --be --over

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,6 @@ const body_parser = require('body-parser');
 const connectionDB = require('./ConnectionDB/DBconfig')
 const opper_model  = require('./Models/Oppertunities_model')
 
-
-connectionDB();
-
 const app = express();
 const port = 3000
 const cors_option = {
@@ -18,20 +15,19 @@ app.use(cors(cors_option));
 app.use(body_parser.json());
 app.use(express.urlencoded({extended: true}))
 
+connectionDB();
 
 
 app.get('/oppertunitiesdata', async (req ,res ) => {
     try {
         const opper_data = await opper_model.find({});
         console.log(opper_data);
-        
-        res.send(opper_data.json);
+        res.send(opper_data)
     } catch (error) {
         console.log(error);
         
     }
 })
-
 
 
 app.listen(port , ()=>{console.log(`the server is listening ${port}`);});
